d061b8c2daebedd0add9897290fb578c53ce5685
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-08-27 09:54:43 -0500

    Added classes

---
+++ Exercises/src/Chapter1exercises/Tablesandchairs.java
@@ -0,0 +1,33 @@
+package Chapter1exercises;
+
+public class Tablesandchairs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Word");
+		//something
+		
+		System.out.println("X                              X");
+		System.out.println("X                              X");
+		System.out.println("X          XXXXXXXX             X");
+		System.out.println("XXX        X      X           XXX ");
+		System.out.println("X X        X      X           X X");
+
+		displayTruck();
+	}
+	
+	
+	public static void displayTruck() {
+		
+		
+		
+		
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
+		System.out.println("X                                XXXXXXXX");
+		System.out.println("X                                       X");
+		System.out.println("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX ");
+		System.out.println("         XOOX           XOOX");
+	
+	}
+
+}
