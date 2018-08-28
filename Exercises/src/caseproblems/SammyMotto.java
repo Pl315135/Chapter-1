23d2b3fbbc51a33b285790343048f05032c518fc
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-08-28 15:45:53 -0500

    caseproblems

---
+++ Exercises/src/caseproblems/SammyMotto.java
@@ -0,0 +1,12 @@
+package caseproblems;
+
+public class SammyMotto {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+System.out.println("S Sammy's makes it fun in the sun  S");
+System.out.println("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+	}
+
+}
