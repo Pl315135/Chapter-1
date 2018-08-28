24ad5af43c33284981af97032d8c0d6f4be38a8a
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-08-28 08:59:21 -0500

    Favorite Qoute

---
+++ Exercises/src/Chapter1exercises/FavoriteSong.java
@@ -0,0 +1,14 @@
+package Chapter1exercises;
+
+public class FavoriteSong {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Song: Hot Cross Buns");
+		System.out.println("Line 1: hot cross cuns");
+		System.out.println("Line 2: hot cross buns");
+		System.out.println("Line 3: One a penny, two a penny");
+		System.out.println("Line 4: hot cross buns");
+	}
+
+}
---
+++ Exercises/src/Chapter1exercises/MovieQuoteInfo.java
@@ -0,0 +1,14 @@
+package Chapter1exercises;
+
+public class MovieQuoteInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Quote: Where's my super suit ?");
+		System.out.println(" Movie: The Incredibles       ");
+		System.out.println(" Year: 2004           ");
+		System.out.println(" Character: Frozone        ");
+
+	}
+
+}
