9139df2dc0aa8fbc6bdaad9f1eb05f1d62af979d
Phero Luong <Pl315135@students.davenport.k12.ia.us>
2018-09-18 10:48:07 -0500

    update 2

--- Chapter5/src/practice/Agegame.java
+++ Chapter5/src/practice/Agegame.java
@@ -1,24 +1,31 @@
 	package practice;
 	import java.util.Scanner;
-	public class Agegame {
+	public class Agegame 
+	{
 	
-		public static void main(String[] args) {
+		public static void main(String[] args) 
+		{
 			// TODO Auto-generated method stub
-	int usersAge;
-	String result;
-	Scanner input = new Scanner(System.in);
-	
-	System.out.println("Please enter your age to consulted");
-	usersAge = input.nextInt();
-	if(usersAge >= 12);
-	{
-		result = "you are a child!";
-	}
-	else if(usersAge >= 13 && usersAge <= 18)
-	{
-		result = "you are a teenager";
-	}
-	
+			int usersAge;
+			String result;
+			Scanner input = new Scanner(System.in);
+			
+			System.out.println("Please enter your age to consulted");
+			usersAge = input.nextInt();
+			if(usersAge >= 12)
+			{
+				result = "you are a child!";
+			}
+			else if(usersAge >= 13 && usersAge <= 18)
+			{
+				result = "you are a teenager";
+			}
+			else if(usersAge >= 29 && usersAge <= 25)
+			{
+				result = "Get a job,you bum";
+			}
+			displayMessage()
+		
 		}
 	
 	}
